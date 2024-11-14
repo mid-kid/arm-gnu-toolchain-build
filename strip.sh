2d3f953dcabe234f373660ad7f1cd32d3bc5c240
mid-kid <esteve.varela@gmail.com>
2024-11-14 01:03:57 +0100

    Remove more redundant files

--- strip.sh
+++ strip.sh
@@ -10,6 +10,7 @@ rm -vf lib/*.a lib/*.la lib64/*.a lib64/*.la
 rm -rvf lib/pkgconfig lib64/pkgconfig
 rm -rvf lib/bfd-plugins/*.a lib64/bfd-plugins/*.a
 rm -rvf include
+rm -rvf share/info share/man
 
 strip -v --strip-all --remove-section=.comment bin/* "$ARCH"/bin/* || true
 find libexec -type f -print0 | xargs -0 -r strip -v --strip-all --remove-section=.comment || true
