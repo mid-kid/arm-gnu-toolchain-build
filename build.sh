87057ac7785c69319af48c5e97617b5fe0088c14
mid-kid <esteve.varela@gmail.com>
2026-04-10 01:26:58 +0200

    Update for 15.2.Rel1

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-set -e
+set -eu
 . ./env
 
 mkdir -p build && cd build
 if [ ! -d src ]; then
     mkdir -p src && cd src
     git clone -b $devtools_ver ../../src/gnu-devtools-for-arm
-    tar xvf ../../src/arm-gnu-toolchain-src-snapshot-$ver.tar.xz
+    echo "Extracting arm-gnu-toolchain-src-snapshot-$ver.tar.xz..."
+    tar xf ../../src/arm-gnu-toolchain-src-snapshot-$ver.tar.xz
     cd ..
 fi
 ln -svf src/gnu-devtools-for-arm/build-gnu-toolchain.sh
@@ -14,8 +15,22 @@ ln -svf src/gnu-devtools-for-arm/build-gnu-toolchain.sh
 # Release toolchain commands grabbed from gnu-devtools-for-arm/README.md
 NPROC="${NPROC:-$(nproc)}"
 
-if [ "$1" != aarch64 ]; then
-    ./build-gnu-toolchain.sh --target=arm-none-eabi --aprofile  --rmprofile -- --release --package --enable-newlib-nano --enable-gdb-with-python=yes "-j$NPROC"
-else
-    ./build-gnu-toolchain.sh --target=aarch64-none-elf -- --release --package --enable-gdb-with-python=yes "-j$NPROC"
-fi
+# Downgrade compiler flags that break the build
+mkdir -p bin
+for x in $(uname -m)-none-linux-gnu-gcc gcc; do
+    command -v "$x" > /dev/null || continue
+    echo '#!/bin/sh' > "bin/$x"
+    echo 'exec '"'$(command -v "$x")'"' -std=gnu17 "$@"' >> "bin/$x"
+    chmod +x "bin/$x"
+done
+export PATH="$PWD/bin:$PATH"
+
+case "${1:-arm}" in
+    arm)
+        ./build-gnu-toolchain.sh --target=arm-none-eabi --aprofile  --rmprofile -- --release --package --enable-newlib-nano --enable-gdb-with-python=yes "-j$NPROC"
+        ;;
+
+    aarch64)
+        ./build-gnu-toolchain.sh --target=aarch64-none-elf -- --release --package --enable-gdb-with-python=yes "-j$NPROC"
+        ;;
+esac
